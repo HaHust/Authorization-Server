840e82f4b88dc2fa29e70fb7698b7b69aafe64ba
HaHust <vanha.hust@gmail.com>
2021-12-31 23:24:32 +0700

    first commit

---
+++ src/main/java/com/h2s/authorizationserver/config/AuthServerConfig.java
@@ -0,0 +1,39 @@
+/*
+ * @created 30/12/2021 - 5:21 PM
+ * @author vanha
+ */
+
+package com.h2s.authorizationserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig
+        extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret("secret")
+                .authorizedGrantTypes("authorization_code", "refresh_token")
+                .scopes("read")
+                .redirectUris("http://localhost:9090/home");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+}
